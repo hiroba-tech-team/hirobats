af1fa3da591597d946ea1211fbfab048673840bf
syehacom <kenshicaster@gmail.com>
2021-12-02 23:01:14 +0900

    tanaka

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,51 @@
 import React, { useState } from "react";
-import Link from "next/link";
 import Image from "next/image";
 
+interface Channel {
+  id: number;
+  name: string;
+  desc: string;
+}
+
+interface Dialog {
+  id: number;
+  message: string;
+  time: string;
+  user: string;
+}
+
 export default function Main() {
-  const [messages, setMessages] = useState([
+  const [channels, setChannels] = useState<Channel[]>([
+    { id: 0, name: "#サンプル", desc: "チャンネルの説明です" },
+    { id: 1, name: "打合せ", desc: "いつ始めましょうか" },
+    { id: 2, name: "定例", desc: "いつもの時間で" },
+  ]);
+  const [current, setCurrent] = useState<number>(0);
+  const [user, setUser] = useState<string>("ひろばくん");
+  const [value, setValue] = useState<string>("");
+  const [dialog, setDialog] = useState<Dialog[]>([
     {
-      id: 1,
+      id: 0,
       message: "こんにちは",
       time: "15:10",
+      user: "たなか",
     },
-    { id: 2, message: "はじめまして", time: "16:10" },
+    { id: 0, message: "はじめまして", time: "16:10", user: "たなか" },
+    { id: 1, message: "ありがとう", time: "17:10", user: "たなか" },
+    { id: 1, message: "どういたしまして", time: "18:10", user: "たなか" },
+    { id: 2, message: "こんにちは", time: "19:10", user: "たなか" },
+    { id: 2, message: "さようなら", time: "20:10", user: "たなか" },
   ]);
-  const [channels, setChannels] = useState([
-    { id: 1, name: "#サンプル", desc: "チャンネルの説明です" },
-    { id: 2, name: "打合せ", desc: "" },
-    { id: 3, name: "定例", desc: "" },
-  ]);
-  const [current, setCurrent] = useState(channels[0]);
+
+  const handleOnClick = (e: number) => {
+    setCurrent(e);
+  };
+
+  const handleSubmit = () => {
+    dialog.push({ id: current, message: value, time: "21:20", user: user });
+    setDialog(dialog);
+    setValue("");
+  };
 
   return (
     <>
@@ -31,7 +60,7 @@ export default function Main() {
           style={{
             background: "purple",
             height: "100vh",
-            width: "18vw",
+            width: "18em",
           }}
         >
           <div
@@ -58,24 +87,22 @@ export default function Main() {
                 fontSize: "20px",
               }}
             >
-              ひろばくん
+              {user}
             </div>
           </div>
           <hr />
           <div>
-            <Link href={"/about"} passHref>
-              <div
-                style={{
-                  fontWeight: "bold",
-                  color: "white",
-                  fontSize: "18px",
-                  padding: "10px 15px",
-                  cursor: "pointer",
-                }}
-              >
-                チャンネル一覧
-              </div>
-            </Link>
+            <div
+              style={{
+                fontWeight: "bold",
+                color: "white",
+                fontSize: "18px",
+                padding: "10px 15px",
+                cursor: "pointer",
+              }}
+            >
+              チャンネル一覧
+            </div>
             {channels.map((channel) => {
               return (
                 <div key={channel.id}>
@@ -94,6 +121,7 @@ export default function Main() {
                         fontSize: "20px",
                         cursor: "pointer",
                       }}
+                      onClick={() => handleOnClick(channel.id)}
                     >
                       {channel.name}
                     </li>
@@ -104,7 +132,7 @@ export default function Main() {
           </div>
         </div>
 
-        <div style={{ width: "100vw" }}>
+        <div style={{ width: "100vw", position: "relative" }}>
           <div
             style={{
               padding: "10px 20px",
@@ -119,7 +147,7 @@ export default function Main() {
                 marginBottom: "10px",
               }}
             >
-              {current.name}
+              {channels[current].name}
             </div>
             <div
               style={{
@@ -127,79 +155,165 @@ export default function Main() {
                 marginBottom: "10px",
               }}
             >
-              {current.desc}
+              {channels[current].desc}
             </div>
           </div>
-          <div style={{ paddingBottom: "90px" }}>
-            {messages.map((message) => {
-              return (
-                <div key={message.id}>
-                  <div
-                    style={{
-                      display: "flex",
-                      marginBottom: "20px",
-                      paddingLeft: "20px",
-                      marginRight: "10px",
-                    }}
-                  >
-                    <Image
-                      src="https://ca.slack-edge.com/T0188513NTW-U01867WD8GK-ga631e27835b-72"
-                      alt=""
-                      height={40}
-                      width={60}
-                    />
-                    <div>
-                      <div
-                        style={{
-                          marginLeft: "10px",
-                          display: "flex",
-                          alignItems: "center",
-                          marginBottom: "5px",
-                        }}
-                      >
-                        <span>{message.time}</span>
-                      </div>
-                      <div
-                        style={{
-                          marginLeft: "20px",
-                          color: "black",
-                          fontSize: "20px",
-                        }}
-                      >
-                        {message.message}
-                      </div>
+
+          <div style={{ width: "70vw" }}>
+            <div style={{ paddingBottom: "90px" }}>
+              {dialog.map((e, idx) => {
+                if (e.id === current) {
+                  return (
+                    <div key={idx}>
+                      {user === e.user ? (
+                        <div>
+                          <div style={{ textAlign: "right" }}>
+                            <div>
+                              <div
+                                style={{
+                                  display: "flex",
+                                  right:"0"
+                                }}
+                              >
+                                <div
+                                  style={{
+                                    position: "relative",
+                                    display: "inline-block",
+                                    padding: "10px 10px",
+                                    minWidth: "120px",
+                                    maxWidth: "50%",
+                                    color: "black",
+                                    fontSize: "20px",
+                                    background: "skyblue",
+                                    marginRight: "30px",
+                                  }}
+                                >
+                                  {e.message}
+                                </div>
+                              </div>
+                              <div>
+                                <Image
+                                  src="https://ca.slack-edge.com/T0266FRGM-U2Q173U05-g863c2a865d7-512"
+                                  alt=""
+                                  height={60}
+                                  width={60}
+                                />
+                              </div>
+                            </div>
+                          </div>
+                          <div
+                            style={{
+                              marginLeft: "10px",
+                              display: "flex",
+                              marginBottom: "5px",
+                              right: "0",
+                            }}
+                          >
+                            <span>{e.time}</span>
+                          </div>
+                        </div>
+                      ) : (
+                        <div style={{ textAlign: "left" }}>
+                          <div
+                            style={{
+                              marginBottom: "2px",
+                              paddingLeft: "20px",
+                              marginRight: "10px",
+                              fontWeight: "bold",
+                            }}
+                          >
+                            {e.user}
+                          </div>
+                          <div
+                            style={{
+                              display: "flex",
+                              marginBottom: "20px",
+                              paddingLeft: "20px",
+                              marginRight: "10px",
+                            }}
+                          >
+                            <Image
+                              src="https://ca.slack-edge.com/T0188513NTW-U01867WD8GK-ga631e27835b-72"
+                              alt=""
+                              height={60}
+                              width={60}
+                            />
+                            <div>
+                              <div
+                                style={{
+                                  marginLeft: "10px",
+                                  display: "flex",
+                                  alignItems: "center",
+                                  marginBottom: "5px",
+                                }}
+                              >
+                                <span>{e.time}</span>
+                              </div>
+                              <div
+                                style={{
+                                  marginLeft: "20px",
+                                  color: "black",
+                                  fontSize: "20px",
+                                }}
+                              >
+                                {e.message}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )}
                     </div>
-                  </div>
+                  );
+                }
+              })}
+              {dialog.length === 0 && (
+                <div style={{ textAlign: "center", marginTop: 20 }}>
+                  メッセージがありません
                 </div>
-              );
-            })}
-            {messages.length === 0 && (
-              <div style={{ textAlign: "center", marginTop: 20 }}>
-                メッセージがありません
-              </div>
-            )}
+              )}
+            </div>
           </div>
 
-          <div
-            style={{
-              position: "fixed",
-              bottom: "0",
-            }}
-          >
-            <textarea
-              placeholder="メッセージを入力してください"
-              // onChange={null}
-              // onKeyDown={null}
-              // value={null}
+          <div>
+            <div
               style={{
-                fontSize: "20px",
-                width: "80vw",
-                marginLeft: "10px",
-                height: "70px",
-                padding: "5px",
-                marginBottom: "10px",
+                position: "absolute",
+                bottom: "0",
+                display: "column",
               }}
-            ></textarea>
+            >
+              <textarea
+                placeholder="メッセージを入力してください"
+                onChange={(e) => setValue(e.target.value)}
+                value={value}
+                style={{
+                  fontSize: "20px",
+                  width: "70vw",
+                  marginLeft: "10px",
+                  height: "100px",
+                  padding: "5px",
+                  marginBottom: "-50px",
+                }}
+              ></textarea>
+              <button
+                style={{
+                  fontSize: "15pt",
+                  width: "10vw",
+                  height: "70px",
+                  marginLeft: "20px",
+                  borderRadius: "50px",
+                  cursor: "pointer",
+                  padding: "12px 12px",
+                  color: "purple",
+                  background: "white",
+                  border: "1px solid black",
+                  marginBottom: "30px",
+                }}
+                onClick={handleSubmit}
+              >
+                送信する
+              </button>
+            </div>
           </div>
         </div>
       </div>
